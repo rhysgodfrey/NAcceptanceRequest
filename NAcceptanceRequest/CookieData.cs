5d0c23a0cecde3b290ac17ff6fca29f13d48b46a
Rhys Godfrey <rhys@rhysgodfrey.co.uk>
2012-06-04 12:08:47 +0100

    Added ability to send cookies

---
+++ NAcceptanceRequest/CookieData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAcceptanceRequest
+{
+    public class CookieData
+    {
+        public CookieData(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+    }
+}
---
+++ NAcceptanceRequest/GetRequestWithCookies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace NAcceptanceRequest
+{
+    public class GetRequestWithCookies : GetRequest
+    {
+        public GetRequestWithCookies(string url, IEnumerable<CookieData> cookies)
+            : base(url)
+        {
+            Request.CookieContainer = new CookieContainer();
+
+            foreach (CookieData cookie in cookies)
+            {
+                Request.CookieContainer.Add(new Cookie(cookie.Key, cookie.Value));
+            }
+        }
+    }
+}
