b4ec5f17938ca99ea08a845429a3e56061fbaef2
Rhys Godfrey <rhys@rhysgodfrey.co.uk>
2012-08-26 12:57:07 +0100

    Adding ability to switch user agent

---
+++ NAcceptanceRequest/GetRequestWithUserAgent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAcceptanceRequest
+{
+    public class GetRequestWithUserAgent : GetRequest
+    {
+        public GetRequestWithUserAgent(string url, string userAgent) :
+            base(url)
+        {
+            Request.UserAgent = userAgent;
+        }
+    }
+}
